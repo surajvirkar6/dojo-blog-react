4f764efaddd5042cebd4a4aad82443580aeecfb3
Suraj Virkar <surajvirkar6@gmail.com>
2022-09-13 09:01:32 +0530

    first commit

---
+++ src/useFetch.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const abortCont = new AbortController();
+
+        setTimeout(() => {
+            fetch(url, { signal: abortCont.signal })
+                .then(res => {
+                    if (!res.ok) {
+                        throw Error("could not fetch");
+                    }
+                    return res.json();
+                })
+                .then((data) => {
+                    setData(data);
+                    setIsPending(false);
+                    setError(null);
+                }).catch((e) => {
+                    if(e.name === "AbortError"){
+                        console.log('fetch aborted.');
+                    }else {
+                    setError(e.message);
+                    setIsPending(false);
+                    }
+                });
+        }, 1000)
+
+        return () => abortCont.abort();
+    }, [url]);
+
+    return { data, isPending, error }
+}
+
+export default useFetch;
